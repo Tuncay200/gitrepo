5035092dbc49cc9b6e1213f7338096e14d61ef18
Tuncay200 <tuncayadisirinli528@gmail.com>
2024-05-15 14:30:38 +0400

    feature2

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public  static void  main(){
         System.out.println("Hello, world");
 
-        System.out.println("Feature1");
+        System.out.println("Feature2");
     }
 }
